971e854649fd9402731c6b64c46bb4f9dda9b3cd
Ayu Nakata <ayu.nakata@mugenup.com>
2025-01-07 00:37:42 +0900

    fix: 確率計算を別ブロックに移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
           <BinarySearch />
           <CoinToss />
           <RunLengthEncoding />
+          <H1>確率計算</H1>
+          <CoinToss />
         </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
