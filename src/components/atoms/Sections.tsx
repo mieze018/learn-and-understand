932bf000cd200358c8b474d94e25bbe1b9350c16
Ayu Nakata <ayu.nakata@mugenup.com>
2025-02-09 05:53:34 +0900

    feat: Section3コンポーネントに縦方向のスタイルオプションを追加

--- src/components/atoms/Sections.tsx
+++ src/components/atoms/Sections.tsx
@@ -10,6 +10,8 @@ export const Section2 = styled.section.attrs({
     'flex flex-col gap-4 items-center flex-1  w-full flex-wrap justify-center',
 })``;
 
-export const Section3 = styled.section.attrs({
-  className: 'flex gap-4 items-end w-full flex-wrap justify-center',
-})``;
+export const Section3 = styled.section.attrs<{ vertical?: boolean }>(
+  (props) => ({
+    className: `flex gap-4 w-full items-center flex-wrap justify-center ${props.vertical ? 'flex-col' : 'flex-row'}`,
+  }),
+)``;
